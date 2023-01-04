014c04e2de73157ef4b5d6f7738cad50c7dc743e
tranhoangkhoi189@gmail.com <tranhoangkhoi189@gmail.com>
2023-01-04 16:19:11 +0700

    UI Custom Dashboard

--- hk-custom-dashboard/custom_dashboard.php
+++ hk-custom-dashboard/custom_dashboard.php
@@ -1,6 +1,136 @@
 <style>
+	.grid {
+		width: 100%;
+		display: block;
+		padding: 0;
+	}
+
+
+	.row {
+		display: flex;
+		flex-wrap: wrap;
+		margin-left: -12px;
+		margin-right: -12px;
+	}
+
+	.col {
+		padding-left: 12px;
+		padding-right: 12px;
+	}
+
+	.l-4 {
+		flex: 0 0 33.33333%;
+		max-width: 31.33333%;
+		margin: 10px 0;
+	}
+
 	.about-wrap {
 		max-width: unset;
+	}
+
+	.wrap-content {
+		padding: 20px 40px;
+		background-color: #fff;
+		box-shadow: 0px 1px 2px 0px rgb(0 0 0 / 30%);
+		margin: 20px 0;
+	}
+
+	.wrap-content__noBG {
+		/* padding: 20px 40px; */
+		/* background-color: #fff;
+		box-shadow: 0px 1px 2px 0px rgb(0 0 0 / 30%); */
+		margin: 20px 0;
+	}
+
+	.flex-block {
+		display: flex;
+		align-items: center;
+
+		justify-content: center;
+	}
+
+	.icon-noti {
+		font-size: 60px;
+		margin-right: 40px;
+	}
+
+	.sub-content {
+		line-height: 18px;
+		font-size: 15px;
+		color: #555;
+	}
+
+	.noti-content {
+		flex: 1;
+	}
+
+	.noti-content h3 {
+		line-height: 2rem;
+		margin: 0;
+	}
+
+	.title {
+		padding-top: 20px;
+	}
+
+	.title h3 {
+		text-transform: capitalize;
+		font-weight: 500;
+		font-size: 18px;
+	}
+
+	.quict__item {
+		background-color: #fff;
+		display: block;
+		width: 100%;
+		padding: 20px 0;
+		color: #333;
+		text-decoration: none;
+		text-align: center;
+		border: 2px solid transparent;
+		overflow: hidden;
+	}
+
+	.quict__item:hover {
+		color: transparent;
+		border: 2px solid var(--e-context-primary-color);
+	}
+
+	.quict__item--img {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+
+	.quict__item--img img {
+		width: 60%;
+		height: 160px;
+		object-fit: contain;
+	}
+
+
+	.quict__item--title p {
+		text-transform: capitalize;
+		font-weight: 500;
+		font-size: 18px;
+		color: #333;
+		margin: 4px;
+	}
+
+	.quict__item--link {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--e-context-primary-color);
+	}
+
+	.quict__item--link p {
+		margin: 0;
+		text-transform: capitalize;
+	}
+
+	.quict__item--icon {
+		position: relative;
+		top: 1px;
 	}
 </style>
 <?php
@@ -15,36 +145,151 @@ require_once(ABSPATH . 'wp-admin/admin.php');
 require_once(ABSPATH . 'wp-admin/admin-header.php');
 ?>
 <div class="wrap about-wrap">
+	<div class="wrap-content flex-block">
+		<i class="fa-regular fa-bell icon-noti"></i>
+		<div class="noti-content">
+			<h3><?php _e("You haven't finished setting up your site."); ?></h3>
 
-	<h1><?php _e('Chào mừng bạn đến với trang quản trị AME Digital'); ?></h1>
-
-	<div class="about-text">
-		<?php _e('Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'); ?>
+			<div class="sub-content">
+				<?php _e('Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'); ?>
+			</div>
+		</div>
 	</div>
 
-	<h2 class="nav-tab-wrapper">
-		<a href="#" class="nav-tab nav-tab-active">
-			<?php _e('Step 1'); ?>
-		</a><a href="#" class="nav-tab">
-			<?php _e('Step 2'); ?>
-		</a><a href="#" class="nav-tab">
-			<?php _e('Step 3'); ?>
-		</a>
-	</h2>
+	<div class="wrap-content">
+		<h1><?php _e('Chào mừng bạn đến với trang quản trị AME Digital'); ?></h1>
 
-	<div class="changelog">
-		<h3><?php _e('Morbi leo risus, porta ac consectetur'); ?></h3>
-
-		<div class="feature-section images-stagger-right">
-			<img src="<?php echo esc_url(admin_url('images/screenshots/theme-customizer.png')); ?>" class="image-50" />
-			<h4><?php _e('Risus Consectetur Elit Sollicitudin'); ?></h4>
-			<p><?php _e('Cras mattis consectetur purus sit amet fermentum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui.'); ?></p>
-
-			<h4><?php _e('Mattis Justo Purus'); ?></h4>
-			<p><?php _e('Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-
-Cras mattis consectetur purus sit amet fermentum. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam.'); ?></p>
+		<div class="about-text">
+			<?php _e('Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'); ?>
 		</div>
+	</div>
+
+	<div class="title">
+		<h3>
+			Các chức năng
+		</h3>
+	</div>
+
+	<div class="wrap-content__noBG">
+		<div class="grid">
+			<div class="row">
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src=<?php echo plugins_url('/img/text.png', __FILE__) ?> />
+						</div>
+						<div class="quict__item--title">
+							<p>Tùy biến Text</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src="<?php echo plugins_url('/img/image.png', __FILE__) ?>" />
+						</div>
+						<div class="quict__item--title">
+							<p>Tùy biến hình ảnh</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src="<?php echo plugins_url('/img/link.png', __FILE__) ?>" />
+						</div>
+						<div class="quict__item--title">
+							<p>Tùy biến Link</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src="<?php echo plugins_url('/img/color.png', __FILE__) ?> " />
+						</div>
+						<div class="quict__item--title">
+							<p>Tùy biến màu sắc</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src="<?php echo plugins_url('/img/code.png', __FILE__) ?>" />
+						</div>
+						<div class="quict__item--title">
+							<p>Chèn code vào Website</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+				<div class="col l-4">
+					<a href="#" class="quict__item">
+						<div class="quict__item--img">
+							<img src="<?php echo plugins_url('/img/css.png', __FILE__) ?>" />
+						</div>
+						<div class="quict__item--title">
+							<p>Thay đổi CSS</p>
+						</div>
+						<div class="quict__item--link">
+							<p>
+								<span>
+									Xem
+								</span>
+								<i class="fa-solid fa-arrow-right-long quict__item--icon"></i>
+							</p>
+						</div>
+					</a>
+				</div>
+
+			</div>
+		</div>
+
 	</div>
 
 </div>
